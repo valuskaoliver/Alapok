cc3b032ec69f63e9b7c0ef498bad9ac2e4181b47
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-18 11:43:45 +0200

    5.feladat

---
+++ Konyv/Konyvek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konyv
+{
+    internal class Konyvek
+    {
+        public string Cim {  get; set; }
+        public int Oldalszam { get; set; }
+        public int Kiadaseve { get; set; }
+
+        public Konyvek() 
+        {
+
+        }
+        public Konyvek(string cim)
+        {
+            Cim = cim;
+        }
+
+
+        public Konyvek(string cim, int oldalszam, int kidaseve)
+        {
+            Cim = cim;
+            Oldalszam = oldalszam;
+            Kiadaseve = kidaseve;
+        }
+        
+    }
+}
---
+++ Konyv/Program.cs
@@ -0,0 +1,15 @@
+﻿using Konyv;
+Konyvek konyv1 = new Konyvek();
+konyv1.Cim = "A gyűrűk ura";
+konyv1.Oldalszam = 1178;
+konyv1.Kiadaseve = 1954;
+
+Konyvek konyv2 = new Konyvek("A legyek ura");
+konyv2.Oldalszam = 224;
+konyv2.Kiadaseve = 1964;
+
+Konyvek konyv3 = new Konyvek("A kis herceg", 96, 1943);
+
+Console.WriteLine($"{konyv1.Cim}, {konyv1.Oldalszam}, {konyv1.Kiadaseve}");
+Console.WriteLine($"{konyv2.Cim}, {konyv2.Oldalszam}, {konyv2.Kiadaseve}");
+Console.WriteLine($"{konyv3.Cim}, {konyv3.Oldalszam}, {konyv3.Kiadaseve}");

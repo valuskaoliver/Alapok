893904de6cd7db4ffee4d194b46d1bfee20cb744
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-18 11:14:12 +0200

    3.feladat

---
+++ bankszamla/Program.cs
@@ -0,0 +1,6 @@
+﻿using bankszamla;
+
+bankszamlak szamla = new bankszamlak();
+//szamla.egyenleg = 10000;
+szamla.befizetes(10000);
+szamla.Kivet(3000);
---
+++ bankszamla/bankszamlak.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankszamla
+{
+    internal class bankszamlak
+    {
+        private double egyenleg;
+        public void befizetes(double osszeg)
+        {
+            if (osszeg > 0) 
+            {
+                egyenleg += osszeg;
+            }
+            else Console.WriteLine("Nem lehet negativ");
+        }
+        public void Kivet(double osszeg) 
+        {
+            if ((osszeg > 0) && egyenleg >= osszeg)
+            {
+                egyenleg -= osszeg;
+            }
+            else Console.WriteLine("Nem vehetsz le tobbet ami van");
+        }
+    }
+}

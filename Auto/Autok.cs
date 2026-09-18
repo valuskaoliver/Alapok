e2086c604b226ba4419d3529b1a2cf65aa4085fb
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-18 10:25:55 +0200

    1.feladat

---
+++ Auto/Autok.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto
+{
+    internal class Autok
+    {
+        public string Marka;
+        public int Eletkor;
+    }
+}
--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Auto;
+
+Autok kek = new Autok();
+kek.Marka = "Toyota";
+kek.Eletkor = 5;
+Autok zold = new Autok();
+Console.Write("Kérem az autó márkáját");
+zold.Marka = Console.ReadLine();
+Console.Write("Kérem az autó életkorát");
+zold.Eletkor = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A kék autó márkája: {kek.Marka} és életkora: {kek.Eletkor}");
+Console.WriteLine($"A zöld autó márkája: {zold.Marka} és életkora: {zold.Eletkor}");

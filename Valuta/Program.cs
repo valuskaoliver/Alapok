4fdd68c2ef8c5cfa88347661e851748c04dae990
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-18 10:41:27 +0200

    2.feladat

---
+++ Valuta/Program.cs
@@ -0,0 +1,11 @@
+﻿using Valuta;
+
+ValutaValto marcius = new ValutaValto();
+marcius.Arfolyam = 385.5;
+Console.WriteLine($"Euró: 100 Árfolyam:{marcius.Arfolyam}, huf: {marcius.EurorolHuf(100)}");
+ValutaValto junius = new ValutaValto();
+junius.Arfolyam = 390.5;
+Console.WriteLine($"Euró: 100 Árfolyam:{junius.Arfolyam}, huf: {junius.EurorolHuf(100)}");
+
+double fixHuf = ValutaValto.FixErorolHuf(100);
+Console.WriteLine($"Euró: 100, Árfolyam fix: 400, HuF: {fixHuf}");
---
+++ Valuta/ValutaValto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valuta
+{
+    internal class ValutaValto
+    {
+        public double Arfolyam;
+        public double EurorolHuf(double euro)
+        {
+            return euro * Arfolyam;
+        }
+        public static double FixErorolHuf(double euro) 
+        {
+            return euro * 400;
+        }
+    }
+}

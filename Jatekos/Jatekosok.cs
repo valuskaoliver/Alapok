5461220c9dea72f34da7747675c610fe339ba6b3
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-18 11:29:02 +0200

    4.feladat

---
+++ Jatekos/Jatekosok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Jatekos
+{
+    internal class Jatekosok
+    {
+        private int elet;
+        public string Nev { get; set;  }
+        public int Elet 
+        { 
+            get { return elet; }
+            set
+            {
+                if (value < 0) elet = 0;
+                else if (value > 100) elet = 100;
+                else elet = value;
+            }
+        }
+    }
+}
---
+++ Jatekos/Program.cs
@@ -0,0 +1,12 @@
+﻿using Jatekos;
+
+Jatekosok jatekos1 = new Jatekosok();
+jatekos1.Nev = "Béla";
+jatekos1.Elet = 300;
+
+Jatekosok jatekos2 = new Jatekosok();
+jatekos2.Nev = "József";
+jatekos2.Elet = -50;
+
+Console.WriteLine($"{jatekos1.Nev}: {jatekos1.Elet} élete");
+Console.WriteLine($"{jatekos2.Nev}: {jatekos2.Elet} élete");
